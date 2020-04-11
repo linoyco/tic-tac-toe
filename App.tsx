464d7ab7c72d652d62a7a4323f57b1b9fea182c3
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-04-12 00:25:26 +0300

    changes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const list: Array<string> = ['hii', 'bii', 'lii', 'nii', 'dii'];
+
 export default function App() {
+
+  const showList = list.map((item, idx) => (<Text key={idx}>{item}</Text>));
+
   return (
     <View style={styles.container}>
       <Text>Hello Linoy!</Text>
+      {showList}
     </View>
   );
 }
