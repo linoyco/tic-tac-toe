9208f421b794aaff19e94e0d315de00fed3bc1be
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-04-17 19:53:34 +0300

    cross and first user touch works

--- src/Containers/Game.tsx
+++ src/Containers/Game.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, TouchableWithoutFeedback } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableWithoutFeedback, GestureResponderEvent } from 'react-native';
 import * as routes from '../routes';
 import AppButton from '../Components/AppButton';
 import Cross from '../Components/Cross';
@@ -24,29 +24,55 @@ const StyledBoard: any = styled(View)`
 `;
 
 const pointsLocation = [
-    { left: '10px', top: '10px' },
-    { left: '10px', top: '110px' },
-    { left: '10px', top: '210px' },
-    { left: '110px', top: '10px' },
-    { left: '110px', top: '110px' },
-    { left: '110px', top: '210px' },
     { left: '210px', top: '10px' },
+    { left: '110px', top: '10px' },
+    { left: '10px', top: '10px' },
     { left: '210px', top: '110px' },
-    { left: '210px', top: '210px' }
+    { left: '110px', top: '110px' },
+    { left: '10px', top: '110px' },
+    { left: '210px', top: '210px' },
+    { left: '110px', top: '210px' },
+    { left: '10px', top: '210px' }
+];
+
+const touchedLocation = [
+    { startLeft: 0, startTop: 0, endLeft: 100, endTop: 100, id: 0 },
+    { startLeft: 100, startTop: 0, endLeft: 200, endTop: 100, id: 1 },
+    { startLeft: 200, startTop: 0, endLeft: 300, endTop: 100, id: 2 },
+
+    { startLeft: 0, startTop: 100, endLeft: 100, endTop: 200, id: 3 },
+    { startLeft: 100, startTop: 100, endLeft: 200, endTop: 200, id: 4 },
+    { startLeft: 200, startTop: 100, endLeft: 300, endTop: 200, id: 5 },
+
+    { startLeft: 0, startTop: 200, endLeft: 100, endTop: 300, id: 6 },
+    { startLeft: 100, startTop: 200, endLeft: 200, endTop: 300, id: 7 },
+    { startLeft: 200, startTop: 200, endLeft: 300, endTop: 300, id: 8 }
 ];
 
 const Game: React.FunctionComponent = () => {
+    const [userInputs, setUserInputs] = useState([]);
+
+    const handleTouch = (e: GestureResponderEvent) => {
+        e.preventDefault();
+        const { locationX, locationY } = e.nativeEvent;
+        const currentLocation = touchedLocation.find(i =>
+            (locationX >= i.startLeft && locationX <= i.endLeft) && (locationY >= i.startTop && locationY <= i.endTop));
+        if (currentLocation && userInputs.every(i => i !== currentLocation.id)) {
+            setUserInputs(userInputs.concat(currentLocation.id));
+        }
+    }
+
     return (
         <StyledView>
-            <TouchableWithoutFeedback onPress={(e) => console.log('GameBoard ', e.nativeEvent)}>
+            <TouchableWithoutFeedback onPress={(e: GestureResponderEvent) => handleTouch(e)}>
                 <StyledBoard>
                     <View style={{ position: 'absolute', height: 2, width: 298, backgroundColor: 'black', marginTop: 100 }} />
                     <View style={{ position: 'absolute', height: 2, width: 298, backgroundColor: 'black', marginTop: 200 }} />
                     <View style={{ position: 'absolute', height: 298, width: 2, backgroundColor: 'black', marginLeft: 100 }} />
                     <View style={{ position: 'absolute', height: 298, width: 2, backgroundColor: 'black', marginLeft: 200 }} />
                     {
-                        pointsLocation.map((one, idx) => (
-                            <Cross key={idx} marginLeft={one.left} marginTop={one.top} />
+                        userInputs.map((one, idx) => (
+                            <Cross key={idx} marginLeft={pointsLocation[one].left} marginTop={pointsLocation[one].top} />
                         ))
                     }
                 </StyledBoard>
