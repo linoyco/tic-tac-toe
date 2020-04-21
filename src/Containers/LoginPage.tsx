7dfbc4ae6baaa7eb93d4e9fb5f8fdf6c15dbebdd
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-04-21 16:14:49 +0300

    login design and form

--- src/Containers/LoginPage.tsx
+++ src/Containers/LoginPage.tsx
@@ -1,46 +1,124 @@
+// const LoginPage = () => {
+//     const dispatch: Dispatch = useDispatch();
+
+//     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+//     const [phone, setPhone] = useState<string>('');
+//     const [name, setName] = useState<string>('');
+
+//     return (
+//         // <StyledView>
+//         //     <StyledHeader>LOGIN</StyledHeader>
+
+//         //         <CustomInput
+//         //             mode='flat'
+//         //             label='Your phone number'
+//         //         />
+//         //         <CustomInput
+//         //             mode='flat'
+//         //             label='Your name'
+//         //         />
+//         //     <AppLinkButton style={{ marginTop: 100 }} title='Log-in !' linkTo={`${routes.PROFILE}`} textColor='#6200ee' borderColor='#6200ee' />
+//         // </StyledView>
+//     );
+// }
+
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import styled from 'styled-components';
-import * as routes from '../routes';
-import AppLinkButton from '../Components/AppLinkButton';
-import CustomInput from '../Components/CustomInput';
+import { View, StyleSheet, ScrollView, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
+import FormBuilder from 'react-native-paper-form-builder';
+import { useForm } from 'react-hook-form';
+import { Button } from 'react-native-paper';
 
-const StyledView: any = styled(View)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-`;
+interface ILoginDetails {
+    phoneNumber: string;
+    fullName: string;
+}
 
-const StyledHeader = styled(Text)`
-    font-weight: bold;
-    font-size: 30px;
-`;
+const LoginPage: React.FunctionComponent = () => {
 
-const LoginPage = () => {
     const dispatch: Dispatch = useDispatch();
 
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
     const [phone, setPhone] = useState<string>('');
     const [name, setName] = useState<string>('');
-    
+
+
+    const form = useForm({
+        defaultValues: {
+            phoneNumber: '',
+            fullName: '',
+        },
+        mode: 'onChange',
+    });
+
     return (
-        <StyledView>
-            <StyledHeader>LOGIN</StyledHeader>
-            <CustomInput
-                mode='flat'
-                label='Your phone number'
-            />
-            <CustomInput
-                mode='flat'
-                label='Your name'
-            />
-            <AppLinkButton style={{marginTop: 100}} title='Log-in !' linkTo={`${routes.PROFILE}`} textColor='#6200ee' borderColor='#6200ee' />
-        </StyledView>
+        <View style={styles.containerStyle}>
+            <ScrollView contentContainerStyle={styles.scrollViewStyle}>
+                <Text style={styles.headingStyle}>LOGIN</Text>
+
+                <FormBuilder
+                    form={form}
+                    formConfigArray={[
+                        {
+                            type: 'input',
+                            name: 'phoneNumber',
+                            label: 'Your phone number',
+                            rules: {
+                                required: {
+                                    value: true,
+                                    message: 'Phone number is required',
+                                },
+                            },
+                            textInputProps: {
+                                keyboardType: 'phone-pad',
+                                autoCapitalize: 'none',
+                            },
+                        },
+                        {
+                            type: 'input',
+                            name: 'fullName',
+                            label: 'Your full name',
+                            rules: {
+                                required: {
+                                    value: true,
+                                    message: 'Full name is required',
+                                },
+                            },
+                            textInputProps: {
+                                // secureTextEntry: true,
+                                keyboardType: 'name-phone-pad',
+                                autoCapitalize: 'none',
+                            },
+                        },
+                    ]}>
+                    <Button
+                        mode={'contained'}
+                        onPress={form.handleSubmit((data: ILoginDetails) => {
+                            console.log('login details: ', data);
+                        })}>Log-in</Button>
+                </FormBuilder>
+            </ScrollView>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flex: 1,
+        width: '80%',
+    },
+    scrollViewStyle: {
+        flex: 1,
+        padding: 15,
+        justifyContent: 'center',
+    },
+    headingStyle: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginBottom: 40,
+        fontWeight: 'bold'
+    },
+});
 
 export default LoginPage;
