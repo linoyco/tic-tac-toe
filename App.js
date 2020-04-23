6eaf34dce4824c64188d155b447774deb87dcf5c
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2020-04-23 16:57:37 +0300

    change

--- App.js
+++ App.js
@@ -6,6 +6,20 @@ import createSagaMiddleware from 'redux-saga';
 import AppRouter from './src/Containers/AppRouter';
 import { rootReducer } from './src/State/Reducers/index';
 import rootSaga from './src/State/Sagas/index';
+import styled from 'styled-components';
+import { View } from 'react-native';
+
+const StyledView = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  align-content: stretch;
+  
+  width: 100%;
+  height: 100%
+  border: 2px solid red;
+`;
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,7 +33,9 @@ sagaMiddleware.run(rootSaga);
 
 const App = () => (
   <Provider store={store}>
+    <StyledView>
       <AppRouter />
+      </StyledView>
   </Provider>
 );
 
