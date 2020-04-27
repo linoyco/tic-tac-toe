581e835b81a442d68d32f6859adeb999068ab45e
Chen Ben Ami <chen24201@gmail.com>
2020-04-27 18:31:52 +0300

    login ui test with formik

--- cypress/integration/examples/login_spec.js
+++ cypress/integration/examples/login_spec.js
@@ -1,17 +1,23 @@
 describe("Test login and logout page", () => {
     it("login", () => {
-        //Arrange
-        //visit a page
         cy.visit("https://localhost:19006/")
-        //find an element
-        // Act
-        //interact with element
         cy.contains('Please login').click()
-        // Assert
-        // make an assertion about the changes
         // cy.url().should('include', '/commands/actions')
-
-        //find the email input by class not id and types an email and check if the value is the same
-        cy.get('your phone number').type('0502009298').should('have.value', '0502009298')
+        cy.contains('Submit').click()
+        cy.get('[data-testid=fullName-error]').contains("Required")
+        cy.get('[data-testid=phoneNumber-error]').contains("Required")
+        cy.get('[data-testid=phoneNumber-input]').type('050').should('have.value', '050')
+        cy.get('[data-testid=fullName-input]').type('C').should('have.value', 'C')
+        cy.contains('Submit').click()
+        cy.get('[data-testid=phoneNumber-error]').contains("Too Short!")
+        cy.get('[data-testid=fullName-error]').contains("Too Short!")
+        cy.get('[data-testid=phoneNumber-input]').clear().type('05020092981').should('have.value', '05020092981')
+        cy.get('[data-testid=fullName-input]').clear().type('zxcvbnmasdzxcvbnmasdzxcvbnmasdzxcvbnmasdzxcvbnmasda').should('have.value', 'zxcvbnmasdzxcvbnmasdzxcvbnmasdzxcvbnmasdzxcvbnmasda')
+        cy.contains('Submit').click()
+        cy.get('[data-testid=phoneNumber-error]').contains("Too Long!")
+        cy.get('[data-testid=fullName-error]').contains("Too Long!")
+        cy.get('[data-testid=phoneNumber-input]').clear().type('0502009298').should('have.value', '0502009298')
+        cy.get('[data-testid=fullName-input]').clear().type("Chen Ben Ami").should("have.value",'Chen Ben Ami')
+        cy.contains('Submit').click()
     })
 })
--- src/Containers/LoginPage.tsx
+++ src/Containers/LoginPage.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
-import FormBuilder from 'react-native-paper-form-builder';
-import { useForm } from 'react-hook-form';
-
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { loginRequest } from '../State/Actions/App/index';
-import AppLinkButton from '../Components/AppLinkButton';
-
 export interface ILoginDetails {
     phoneNumber: string;
     fullName: string;
@@ -18,71 +15,61 @@ const LoginPage: React.FunctionComponent = () => {
     const dispatch: Dispatch = useDispatch();
 
     const errorMessage = useSelector((state: any) => state.app.errorMessage);
-
-    const form = useForm<ILoginDetails>({
-        defaultValues: {
-            phoneNumber: '',
-            fullName: '',
-        },
-        mode: 'onChange',
-    });
-
     const handleLogin = (loginDetails: ILoginDetails) => {
         dispatch(loginRequest(loginDetails));
     }
 
+    const SignupSchema = Yup.object().shape({
+        fullName: Yup.string()
+            .min(2, 'Too Short!')
+            .max(50, 'Too Long!')
+            .required('Required'),
+        phoneNumber: Yup.string()
+            .min(10, 'Too Short!')
+            .max(10, 'Too Long!')
+            .required('Required'),
+    });
+
+    const form = () => (
+        <Formik
+            initialValues={{ phoneNumber: '', fullName: '' }}
+            onSubmit={(values) => {
+                handleLogin(values)
+            }}
+            validationSchema={SignupSchema}
+        >
+            {({ errors, touched, handleChange, handleBlur, handleSubmit, values }) => (
+                <View>
+                    <TextInput
+                        testID={"phoneNumber-input"}
+                        onChangeText={handleChange('phoneNumber')}
+                        onBlur={handleBlur('phoneNumber')}
+                        value={values.phoneNumber}
+                        placeholder="Enter your phone Number"
+                    />
+                    {errors.phoneNumber && touched.phoneNumber ? (
+                        <Text testID={"phoneNumber-error"}>{errors.phoneNumber}</Text>
+                    ) : null}
+                    <TextInput
+                        testID={"fullName-input"}
+                        onChangeText={handleChange('fullName')}
+                        onBlur={handleBlur('fullName')}
+                        value={values.fullName}
+                        autoCapitalize="words"
+                        placeholder="Enter your full name"
+                    />
+                    {errors.fullName && touched.fullName ? (
+                        <Text testID={"fullName-error"}>{errors.fullName}</Text>
+                    ) : null}
+                    <Button testID={"submit-button"} onPress={handleSubmit} title="Submit" />
+                </View>
+            )}
+        </Formik>
+    );
     return (
         <View style={styles.container}>
             <Text style={styles.headingStyle}>LOGIN</Text>
-
-            <FormBuilder
-                form={form}
-                formConfigArray={[
-                    {
-                        type: 'input',
-                        variant: 'outlined',
-                        name: 'phoneNumber',
-                        label: 'Your phone number',
-                        rules: {
-                            minLength: {
-                                value: 10,
-                                message: 'Phone number is min 10 numbers'
-                            },
-                            required: {
-                                value: true, 
-                                message: 'Phone number is required',
-                            },
-                        },
-                        textInputProps: {
-                            textContentType: 'telephoneNumber',
-                            keyboardType: 'phone-pad',
-                            style: { backgroundColor: 'white' },
-                        },
-                    },
-                    {
-                        type: 'input',
-                        variant: 'outlined',
-                        name: 'fullName',
-                        label: 'Your full name',
-                        rules: {
-                            required: {
-                                value: true,
-                                message: 'Full name is required',
-                            },
-                        },
-                        textInputProps: {
-                            textContentType: 'username',
-                            keyboardType: 'default',
-                            autoCapitalize: 'sentences',
-                            style: { backgroundColor: 'white' },
-                        },
-                    },
-                ]}>
-                <AppLinkButton
-                    title='Log-in'
-                    color='#6200ee'
-                    onPress={form.handleSubmit((data: ILoginDetails) => handleLogin(data))} />
-            </FormBuilder>
+            {form()}
             <Text style={{ color: 'red' }}>{errorMessage || ''}</Text>
         </View>
     );
